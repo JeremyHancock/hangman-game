8dc1170dbe24011d9db30271420e8d1bf7ff5e4f
doulos tou Christou <jeremyhancock32@gmail.com>
2018-08-17 13:02:54 -0400

    making progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,26 +8,23 @@ var guessesLeft = 0;
 var wins = 0;
 var hiddenWord = [];
 //var guessedLetters = document.getElementById("guessLetters");
-//var guessesLeft = document.getElementById("numGuessesLeft");
 //var wins = document.getElementById("winsTotal");
 //var hiddenWord = document.getElementById("guessThisWord");
 
 function startGame() {
-    // var guessesLeft = document.getElementById("numGuessesLeft");
-    // guessesLeft.innerHTML = "10";
-
-    // console.log(guessesLeft);
+    var guessesLeft = document.getElementById("numGuessesLeft");
+    guessesLeft.innerHTML = 10;
     guessedLetters = [];
     hiddenWord = [];
     positions = [];
     currentWord = Math.floor(Math.random() * (dictionary.length));
     currentWord = (dictionary[currentWord]);
     for (var i = 0; i < currentWord.length; i++) {
-        hiddenWord.push("_");
+        hiddenWord.push(" _ ");
 
     }
-    console.log(currentWord);
-    console.log(hiddenWord); 
+    var hiddenWordMarks = document.getElementById("guessThisWord");
+    hiddenWordMarks.innerHTML = hiddenWord;
 };
 
 function convert() {
@@ -36,23 +33,29 @@ function convert() {
 
 function evaluateGuess() {
     if (currentWord.indexOf(currentGuess) > -1) {
-
         for (var i = 0; i < currentWord.length; i++) {
             if (currentWord[i] === currentGuess) {
                 positions.push(i);
-                for (var i = 0; i < positions.length; i++) {
-                    hiddenWord[positions[i]] = currentGuess;
+                for (var j = 0; j < positions.length; j++) {
+                    hiddenWord[positions[j]] = currentGuess;
                 }
             }
         }
     }
-
+    
     else {
-        (guessedLetters.push(currentGuess))
-        guessesLeft--;
+            (guessedLetters.push(currentGuess))
+            var guessesLeft = document.getElementById("numGuessesLeft");
+            guessesLeft.innerHTML--;
+        }
+    
+    var guessedLettersArray = document.getElementById("guessLetters");
+    guessedLettersArray.innerHTML = guessedLetters
+    var fillInSpaces = document.getElementById("guessThisWord");
+    fillInSpaces.innerHTML = hiddenWord;
+    if (hiddenWord === currentWord) {
+        youwin();
     }
-    console.log(guessedLetters);
-    console.log(currentGuess, hiddenWord);
 };
 
 
@@ -69,5 +72,4 @@ document.onkeyup = function (event) {
     else if (event.keyCode === 32) {
         startGame();
     }
-    console.log(currentGuess);
 };
