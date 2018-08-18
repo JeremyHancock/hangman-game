9c3b154b9013e81e304bd0ab24e2f8b07c2dae16
doulos tou Christou <jeremyhancock32@gmail.com>
2018-08-17 20:39:50 -0400

    ready to submit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,21 @@
-var dictionary = ["GANDALF", "SAURON", "RIVENDALE", "HOBBITON", "SMEAGOL", "GOLLUM", "MORDOR", "LEGOLAS", "GIMLI", "SAMWISE", "MERIADOC", "PEREGRIN", "ENTMOOT", "UNDERHILL", "BOMBADIL", "LOTHLORIEN", "FRODO", "ARAGORN", "GALADRIEL", "BOROMIR", "FARAMIR", "EOWYN", "ISILDUR", "DENETHOR", "MITHRANDIR", "TREEBEARD", "NAZGUL", "THEODEN", "WORMTONGUE", "ATHELAS", "BUTTERBUR", "SHARKEY", "ELROND"];
+var dictionary = ["GANDALF", "SAURON", "RIVENDELL", "HOBBITON", "SMEAGOL", "GOLLUM", "MORDOR", "LEGOLAS", "GIMLI", "SAMWISE", "MERIADOC", "PEREGRIN", "ENTMOOT", "UNDERHILL", "BOMBADIL", "LOTHLORIEN", "FRODO", "ARAGORN", "GALADRIEL", "BOROMIR", "FARAMIR", "EOWYN", "ISILDUR", "DENETHOR", "MITHRANDIR", "TREEBEARD", "NAZGUL", "THEODEN", "WORMTONGUE", "ATHELAS", "BUTTERBUR", "SHARKEY", "ELROND"];
 var string;
 var positions = [];
 var currentWord;
 var currentGuess;
 var guessedLetters = [];
-var guessesLeft = 0;
-var wins = 0;
 var hiddenWord = [];
-//var guessedLetters = document.getElementById("guessLetters");
-//var wins = document.getElementById("winsTotal");
-//var hiddenWord = document.getElementById("guessThisWord");
 
 function startGame() {
+    document.getElementById("header-image").style.display = "block";
+    document.getElementById("victory-image").style.display = "none";
+    document.getElementById("defeat-image").style.display = "none";
+    document.getElementById("victoryCry").style.display = "none";
+    document.getElementById("defeatCry").style.display = "none";
     var guessesLeft = document.getElementById("numGuessesLeft");
+    var guessedLettersArray = document.getElementById("guessLetters");
     guessesLeft.innerHTML = 10;
+    guessedLettersArray.innerHTML = [];
     guessedLetters = [];
     hiddenWord = [];
     positions = [];
@@ -31,6 +33,25 @@ function convert() {
     currentGuess = String.fromCharCode(currentGuess);
 };
 
+function checkForVictory() {
+    if(hiddenWord.indexOf(" _ ") === -1) {
+        document.getElementById("victory-image").style.display = "block";
+        document.getElementById("header-image").style.display = "none";
+        document.getElementById("victoryCry").style.display = "block";
+        var wins = document.getElementById("winsTotal");
+        wins.innerHTML++;
+        
+    }
+};
+
+function defeat() {
+    console.log("defeat");
+    document.getElementById("defeat-image").style.display = "block";
+    document.getElementById("defeatCry").style.display = "block";
+    document.getElementById("header-image").style.display = "none";
+
+};
+
 function evaluateGuess() {
     if (currentWord.indexOf(currentGuess) > -1) {
         for (var i = 0; i < currentWord.length; i++) {
@@ -38,37 +59,36 @@ function evaluateGuess() {
                 positions.push(i);
                 for (var j = 0; j < positions.length; j++) {
                     hiddenWord[positions[j]] = currentGuess;
+                    positions = [];
                 }
             }
         }
     }
-    
+
     else {
-            (guessedLetters.push(currentGuess))
-            var guessesLeft = document.getElementById("numGuessesLeft");
-            guessesLeft.innerHTML--;
-        }
-    
+        (guessedLetters.push(currentGuess))
+        var guessesLeft = document.getElementById("numGuessesLeft");
+        guessesLeft.innerHTML--;
+        if (guessesLeft.innerHTML == 0){
+            defeat();
+        }    
+    }
     var guessedLettersArray = document.getElementById("guessLetters");
     guessedLettersArray.innerHTML = guessedLetters
     var fillInSpaces = document.getElementById("guessThisWord");
     fillInSpaces.innerHTML = hiddenWord;
-    if (hiddenWord === currentWord) {
-        youwin();
-    }
+    checkForVictory();
+    
 };
-
-
 
 startGame();
 
 document.onkeyup = function (event) {
     if (event.keyCode >= 65 && event.keyCode <= 90) {
-        currentGuess = event.keyCode;
+        (currentGuess = event.keyCode);
         convert(currentGuess);
         evaluateGuess(currentGuess);
-    }
-
+        }
     else if (event.keyCode === 32) {
         startGame();
     }
