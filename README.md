c20930151a35ea27a54fe01e1b3cbd79d650a653
doulos tou Christou <jeremyhancock32@gmail.com>
2018-08-18 21:40:56 -0400

    working and ready to submit. some desired future changes are explained in the readMe

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # hangman-game
+
+All words are people, places, or events from The Lord of The Rings, by J.R.R. Tolkien, with references to the movies, directed by Peter Jackson. There are 33 words, which will be chosen at random by the program.
+
+To play, press any letter. If the letter you select is in the word it will show there, if not, it will appear on the right and remove one of your 10 guesses. Guess all the letters and you win! Guess 10 letters that are not in the word, and you will lose. You can reset the game and be given a new word at any time by pressing the space bar.
+
+
+**In the future, I hope to be able to fix a couple of things.
+
+One, there is no automatic reset so play can continue after both victory and defeat, which creates problems in the wins counter and brings the remaining guesses allowed into negative numbers, respectively.
+
+Two, I'd like to add an image that would be linked to each word guessed correctly, showing the character or place referenced so that people unfamiliar with The Lord of The Rings could learn something and existing fans could be rewarded for their success.
+
+Three, I'd like to add background music and triggered sounds, possibly randomized, for different events. For example, several negative quotes from the movie to play when a guessed letter is not in the word, several positive clips from the movie when a guessed letter is in the word, and a particular character quote for when a word is completed.**
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// Defines all global variables
 var dictionary = ["GANDALF", "SAURON", "RIVENDELL", "HOBBITON", "SMEAGOL", "GOLLUM", "MORDOR", "LEGOLAS", "GIMLI", "SAMWISE", "MERIADOC", "PEREGRIN", "ENTMOOT", "UNDERHILL", "BOMBADIL", "LOTHLORIEN", "FRODO", "ARAGORN", "GALADRIEL", "BOROMIR", "FARAMIR", "EOWYN", "ISILDUR", "DENETHOR", "MITHRANDIR", "TREEBEARD", "NAZGUL", "THEODEN", "WORMTONGUE", "ATHELAS", "BUTTERBUR", "SHARKEY", "ELROND"];
 var string;
 var positions = [];
@@ -6,12 +7,15 @@ var currentGuess;
 var guessedLetters = [];
 var hiddenWord = [];
 
+//This function is run any time the space bar is pressed and when the page first loads
 function startGame() {
+    //It calls IDs from the HTML, hiding pictures that will be shown for victory and defeat, and showing the header image
     document.getElementById("header-image").style.display = "block";
     document.getElementById("victory-image").style.display = "none";
     document.getElementById("defeat-image").style.display = "none";
     document.getElementById("victoryCry").style.display = "none";
     document.getElementById("defeatCry").style.display = "none";
+    //It also empties arrays and resets guesses left for start of the new game
     var guessesLeft = document.getElementById("numGuessesLeft");
     var guessedLettersArray = document.getElementById("guessLetters");
     guessesLeft.innerHTML = 10;
@@ -19,20 +23,23 @@ function startGame() {
     guessedLetters = [];
     hiddenWord = [];
     positions = [];
+    //Here, it selects a word randomly from the available words in dictionary and sets the currentWord
     currentWord = Math.floor(Math.random() * (dictionary.length));
     currentWord = (dictionary[currentWord]);
+    //this puts an underscore in hiddenWord in place of each character from currentWord
     for (var i = 0; i < currentWord.length; i++) {
         hiddenWord.push(" _ ");
 
     }
+    //puts the underscores from hiddenWord in the user-visible, guessThisWord
     var hiddenWordMarks = document.getElementById("guessThisWord");
     hiddenWordMarks.innerHTML = hiddenWord;
 };
-
+    //converts currentGuess from the key code to a string, which is always a single letter
 function convert() {
     currentGuess = String.fromCharCode(currentGuess);
 };
-
+//Checks to see if the hiddenword still contains _ If it doesn't, it displays the victory image and text and hides the header image. It also adds one to the #winsTotal.
 function checkForVictory() {
     if(hiddenWord.indexOf(" _ ") === -1) {
         document.getElementById("victory-image").style.display = "block";
@@ -43,20 +50,22 @@ function checkForVictory() {
         
     }
 };
-
+//When called, this hides the header-image and displays the defeat-image and text.
 function defeat() {
-    console.log("defeat");
     document.getElementById("defeat-image").style.display = "block";
     document.getElementById("defeatCry").style.display = "block";
     document.getElementById("header-image").style.display = "none";
 
 };
-
+//Most of the magic is happening here.
 function evaluateGuess() {
+    //it compares the single character in currentGuess to each character of the currentWord
     if (currentWord.indexOf(currentGuess) > -1) {
         for (var i = 0; i < currentWord.length; i++) {
+            //if any character of currentWord matches currentGuess it moves that index number to positions
             if (currentWord[i] === currentGuess) {
                 positions.push(i);
+                //this places currentGuess in the index number of hiddenWord indentified by the index numbers in positions (I think. OR something like that that makes it work.)
                 for (var j = 0; j < positions.length; j++) {
                     hiddenWord[positions[j]] = currentGuess;
                     positions = [];
@@ -64,15 +73,17 @@ function evaluateGuess() {
             }
         }
     }
-
+//if currentGuess doesn't match any character in currentWord currentGuess is pushed to the guessedLetters variable and guessesLeft is reduced by one
     else {
         (guessedLetters.push(currentGuess))
         var guessesLeft = document.getElementById("numGuessesLeft");
         guessesLeft.innerHTML--;
+        //if guessesLeft equals zero it runs the defeat function
         if (guessesLeft.innerHTML == 0){
             defeat();
         }    
     }
+    //the player visible elements are updated here and the checkForVictory function is run.
     var guessedLettersArray = document.getElementById("guessLetters");
     guessedLettersArray.innerHTML = guessedLetters
     var fillInSpaces = document.getElementById("guessThisWord");
@@ -80,15 +91,16 @@ function evaluateGuess() {
     checkForVictory();
     
 };
-
+//This function is the first thing run when the page loads up
 startGame();
-
+//This event is triggered when a letter is keyed. It assigns that key code to currentGuess and runs the convert and evaluateGuess functions with currentGuess
 document.onkeyup = function (event) {
     if (event.keyCode >= 65 && event.keyCode <= 90) {
         (currentGuess = event.keyCode);
         convert(currentGuess);
         evaluateGuess(currentGuess);
         }
+        //This separates the space bar with its own event, running the startGame function.
     else if (event.keyCode === 32) {
         startGame();
     }
